fc1ea7ee14e867878ce84dd5272eb4d64a68e91b
Gilad Sterman <giladsterman1999@gmail.com>
2023-12-09 19:10:36 +0200

    font change

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,11 +1,16 @@
 export function AppHeader({ selected, setSelected }) {
+
+    function onChangeLang(lang) {
+        setSelected(lang)
+        window.scrollTo(0, 0)
+    }
     return (
         <header className="app-header">
             <nav className={(selected === 'eng') ? 'eng' : ''}>
-                <button className={`language-btn ${(selected === 'heb') ? 'selected' : ''}`} onClick={() => setSelected('heb')}>
+                <button className={`language-btn ${(selected === 'heb') ? 'selected' : ''}`} onClick={() => onChangeLang('heb')}>
                     <img src="https://res.cloudinary.com/dollaguij/image/upload/v1700476247/heb1_rmybm6.png" alt="" />
                 </button>
-                <button className={`language-btn ${(selected === 'eng') ? 'selected' : ''}`} onClick={() => setSelected('eng')}>
+                <button className={`language-btn ${(selected === 'eng') ? 'selected' : ''}`} onClick={() => onChangeLang('eng')}>
                     <img src="https://res.cloudinary.com/dollaguij/image/upload/v1700476247/eng1_rpfhk8.png" alt="" />
                 </button>
             </nav>
